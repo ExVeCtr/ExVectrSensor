aa0d9338cad86783c0f61459318357f2937ee17c
ChSt15 <chrissteffen98@gmail.com>
2023-04-27 21:11:07 +0200

    Added SNSR namespace

--- include/ExVectrSensor.hpp
+++ include/ExVectrSensor.hpp
@@ -1,12 +1,14 @@
 #ifndef EXVECTRDATA_H_
 #define EXVECTRDATA_H_
 
-
-
 namespace VCTR
-{} // namespace VCTR The ExVeCtr Library
+{
 
+    namespace SNSR
+    {
 
+    } // namespace SNSR for sensor stuff
 
+} // namespace VCTR The ExVeCtr Library
 
 #endif
